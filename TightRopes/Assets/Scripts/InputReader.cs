766c5597c8db2d23c7c88aa9130eef81fff1dd46
AIE\s210837 <s210837@students.aie.edu.au>
2022-08-12 15:13:20 +0930

    Intialised NIght Vision - Should be easily adjustable

--- TightRopes/Assets/Scripts/InputReader.cs
+++ TightRopes/Assets/Scripts/InputReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,14 @@ using UnityEngine.InputSystem;
 public class InputReader : MonoBehaviour, Controls.IPlayerActions
 {
     private Controls controls;
+    private bool isWalking;
+
     public Vector2 MovementValue { get; private set; }
     public Vector2 LookValue { get; private set; }
     public bool IsSprinting { get; private set; }
     public bool jump { get; private set; }
+
+    public event Action InteractEvent;
     private void Start()
     {
         controls = new Controls();
@@ -19,6 +24,16 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     public void OnMove1(InputAction.CallbackContext context)
     {
         MovementValue = context.ReadValue<Vector2>();
+        print(MovementValue);
+        if (MovementValue != new Vector2(0, 0))
+        {
+
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 
     public void OnSprint(InputAction.CallbackContext context)
@@ -36,5 +51,11 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     {
         if (context.performed) jump = true;
         else jump = false;
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed) InteractEvent?.Invoke();
+
     }
 }
