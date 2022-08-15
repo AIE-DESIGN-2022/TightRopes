ad47ddd4f7cb0707363105d5a535176b71114498
JacksonH <jacksonhole24@gmail.com>
2022-08-15 21:26:40 +0930

    ADDED Flashlight animation call to script

--- TightRopes/Assets/Scripts/Flashlight.cs
+++ TightRopes/Assets/Scripts/Flashlight.cs
@@ -8,10 +8,15 @@ public class Flashlight : MonoBehaviour
 
     private GameObject light;
 
+    private AnimationManager animManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
         light = GameObject.Find("Torch Light");
+
+        animManager = FindObjectOfType<AnimationManager>();
     }
 
     // Update is called once per frame
@@ -26,11 +31,13 @@ public class Flashlight : MonoBehaviour
         {
             light.SetActive(true);
             flashlightOn = true;
+            animManager.FlashOut();
         }
         else
         {
             light.SetActive(false);
             flashlightOn = false;
+            animManager.FlashIn();
         }
     }
 }
