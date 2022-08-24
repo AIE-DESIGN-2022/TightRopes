fac53c9c63a99077844d5f63eafeaf07884a16ee
AIE\s210837 <s210837@students.aie.edu.au>
2022-08-24 11:59:04 +0930

    FIXED Batteries allowing light on 0

--- TightRopes/Assets/Scripts/InputReader.cs
+++ TightRopes/Assets/Scripts/InputReader.cs
@@ -127,7 +127,7 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
 
     public void OnFlashlightBattery(InputAction.CallbackContext context)
     {
-        if (battery.Batteries >= 0)
+        if (battery.Batteries > 0)
         {
             if (context.performed) replaceFlashlightBattery?.Invoke();
         }
